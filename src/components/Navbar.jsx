67e3bf8e5efa1d9a550a26691c934f0d7473a6ec
CJ Pineda <pinedacj1020@gmail.com>
2024-08-27 09:09:17 +0000

    added arcade tab

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,6 +24,7 @@ export default function Nav() {
     { to: "about", label: "About" },
     { to: "experience", label: "Experience" },
     { to: "projects", label: "Projects" },
+    { to: "arcade", label: "Arcade"},
     { to: "contact", label: "Contact" }
   ];
  
